5123667f71bce35c8146d01032ea0e80ebf94669
Shayna Sims <shaynarsims@yahoo.com>
2020-09-03 15:38:02 -0500

    map a GeoJSON point

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,12 +1,37 @@
 // Documentation:
-// https://leafletjs.com/examples/quick-start/
-// https://docs.mapbox.com/api/maps/#static-tiles
+// https://leafletjs.com/examples/geojson/
+// https://tools.ietf.org/html/rfc7946
 
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+            // @NOTE: GeoJSON data coordinates are set with the 
+            // first parameter as X (longitude) & the second parameter as Y (latitude)
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
