a890c729913b1fdf57b52dff53ffc6649bac12ff
Shayna Sims <shaynarsims@yahoo.com>
2020-09-02 20:10:43 -0500

    create light-yellow circle w/ black lines indicating 300m radius of LA on dark map

--- Mapping_Single_Points/static/js/logic_addCircle.js
+++ Mapping_Single_Points/static/js/logic_addCircle.js
@@ -8,11 +8,15 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Use the circle() function to place a circle on the map
 L.circle([34.0522, -118.2437], {
-    radius: 100 // radius for circle() is measured in meters
+    radius: 300, // 300-meter radius over central LA
+    color: "black", // black outline on circle
+    fillColor: "yellow", // light-yellow inside of circle
+    fillOpacity: 0.3 // adjust opacity
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Change style to a dark map (https://docs.mapbox.com/api/maps/#styles)
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
