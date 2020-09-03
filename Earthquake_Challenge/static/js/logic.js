c359098acfb78ea4f369bfea9b0334b81a1a2870
Shayna Sims <shaynarsims@yahoo.com>
2020-09-03 18:18:30 -0500

    edit the base layer to hold three maps

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -15,10 +15,18 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite": satelliteStreets
+    "Satellite": satelliteStreets,
+    "Dark": dark
 };
 
 // Create the earthquake layer for our map.
